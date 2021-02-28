3505b8581157f4b9b7940eebf07ac9ba250d24c9
Jamie Taniguchi <tekmosis@gmail.com>
2021-02-28 15:31:09 -0800

    Add migration for the widgets table

---
+++ database/migrations/2021_02_28_231814_create_widgets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWidgetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('widgets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 20)->index();
+            $table->string('description', 100)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('widgets');
+    }
+}
